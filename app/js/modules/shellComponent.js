884e3ef91cceab5653d29b610ba75e1539fad6be
csum112 <cataremix15@gmail.com>
2020-05-28 20:42:38 +0300

    Fixed searchbox size desktop & mobile

--- app/js/modules/shellComponent.js
+++ app/js/modules/shellComponent.js
@@ -53,9 +53,7 @@ export class ShellComponent extends HTMLElement {
       .searchbox {
         background: #3B3F54;
         border-radius: 16px;
-        width: 61%;
-        height: 56px;
-        margin-top: 64px;
+        height: 48px;
         display: flex;
         justify-content: center;
         margin-bottom: 2rem;
@@ -64,11 +62,9 @@ export class ShellComponent extends HTMLElement {
     }
 
     .menu-fab{
-        height: 3.5rem;
-        width: 3.5rem;
-        margin-top: 64px;
         margin-left: 32px;
-        margin-right: 32px;
+        margin-right: 16px;
+        height: 50px;
         color: #3B3F54;
         z-index: 11;
         background: none;
@@ -99,31 +95,6 @@ export class ShellComponent extends HTMLElement {
           
     .searchbox > input:focus {
         outline: none;
-    }
-
-    .dots:nth-of-type(1) {
-        opacity: 0.25;
-    
-        position: absolute;
-        bottom: 0%;
-        left: 5%;
-        z-index: -1;
-    }
-    
-    .dots:nth-of-type(2) {
-        opacity: 0.25;
-        position: absolute;
-        bottom: 0%;
-        right: 5%;
-        z-index: -1;
-    }
-    
-    .dots:nth-of-type(3) {
-        opacity: 0.25;
-        position: absolute;
-        top: 10%;
-        left: 10%;
-        z-index: -1;
     }
 
       @media (min-width: 720px) and (orientation:landscape) {
@@ -174,12 +145,25 @@ export class ShellComponent extends HTMLElement {
           box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.04),
               0px 2px 6px rgba(0, 0, 0, 0.04),
               0px 10px 20px rgba(0, 0, 0, 0.04);
-          border-radius: 32px;
+          border-radius: 16px;
       }
 
       .mobile-fab {
           display: none;
       }
+
+      .logo {
+        position: relative;
+        width: 162px;
+        height: 77px;
+        left: 32px;
+        font-family: Zilla Slab Highlight;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 48px;
+        color: rgba(202, 204, 219, 0.3);
+        text-shadow: 0px 0px 1px rgba(0, 0, 0, 0.04), 0px 2px 6px rgba(0, 0, 0, 0.04), 0px 16px 24px rgba(0, 0, 0, 0.06);
+    }
     }
 
     @media (max-width: 720px),(orientation:portrait){
@@ -201,13 +185,17 @@ export class ShellComponent extends HTMLElement {
             animation: go-up 100ms forwards;
         }
 
-        .scrolled-header .searchbox, .scrolled-header .menu-fab {
-            margin-top: -32px;
-        }
+
 
         .scrolled-header {
             background-color: #252a41;
-            height: 128px;
+            height: 70px;
+        }
+
+        .scrolled-header .header-mobile-wrapper {
+            position: fixed;
+            top: 16px;
+
         }
 
 
@@ -219,7 +207,7 @@ export class ShellComponent extends HTMLElement {
             z-index: 10;
         }
         .golden-bigger {
-            padding-top: 12rem;
+            padding-top: 7rem;
             width: 100%;
             display: flex;
             flex-direction: column;
@@ -245,24 +233,22 @@ export class ShellComponent extends HTMLElement {
             top: 0px;
             left: 0px;
         }
-    }
 
-    .logo {
-        position: relative;
-        width: 162px;
-        height: 77px;
-        left: 32px;
-        top: 32px;
-        font-family: Zilla Slab Highlight;
-        font-style: normal;
-        font-weight: bold;
-        font-size: 64px;
-        line-height: 77px;
-        color: rgba(202, 204, 219, 0.3);
-        text-shadow: 0px 0px 1px rgba(0, 0, 0, 0.04), 0px 2px 6px rgba(0, 0, 0, 0.04), 0px 16px 24px rgba(0, 0, 0, 0.06);
+        .logo {
+            position: relative;
+            left: 8px;
+            font-family: Zilla Slab Highlight;
+            font-style: normal;
+            font-weight: bold;
+            font-size: 36px;
+            line-height: 77px;
+            color: rgba(202, 204, 219, 0.3);
+            text-shadow: 0px 0px 1px rgba(0, 0, 0, 0.04), 0px 2px 6px rgba(0, 0, 0, 0.04), 0px 16px 24px rgba(0, 0, 0, 0.06);
+        }
     }
     
     .searchbox-wrapper {
+        height: 50px;
         width: 100%;
         display: flex;
         justify-content: center;
@@ -300,7 +286,7 @@ export class ShellComponent extends HTMLElement {
               </div>
         <div class="header-mobile-wrapper">
             <button class="menu-fab">
-                <i class="fas fa-bars fa-4x"></i>
+                <i class="fas fa-bars fa-2x"></i>
             </button>
         <div class="searchbox-wrapper">
             <div class="searchbox">
