077fc1cfdfa64b82ec0f7261b388b2f92565bd06
csum112 <cataremix15@gmail.com>
2020-05-28 12:45:48 +0300

    Refactored router

--- app/js/main.js
+++ app/js/main.js
@@ -2,11 +2,12 @@ import { ThumbnailComponent } from "/js/modules/thumbnailComponent.js";
 import { AppListComponent } from "/js/modules/appListComponent.js";
 import { ShellComponent } from "/js/modules/shellComponent.js";
 import { ModalComponent } from "/js/modules/modalComponent.js";
-import { Router } from "/js/modules/router.js";
 import { MenuComponent } from "/js/modules/menuComponent.js";
 import { AppMenuComponent } from "/js/modules/app-menu/appMenuComponent.js"
 import { AppCoverComponent } from "/js/modules/app-menu/appCoverComponent.js";
 import { CheckoutComponent } from "/js/modules/checkout-menu/checkoutComponent.js";
+import { Router } from "/js/routing/router.js";
+import {injectRoutes} from "/js/routing/routes.js"
 
 customElements.define("dm-thumbnail", ThumbnailComponent);
 customElements.define("dm-app-list", AppListComponent);
@@ -18,10 +19,5 @@ customElements.define("dm-app-cover", AppCoverComponent);
 customElements.define("dm-checkout", CheckoutComponent);
 
 const router = new Router();
-router.get("/", (req) => {
-  const root = document.getElementById("root");
-  const template = document.getElementById("main").content.cloneNode(true);
-  root.appendChild(template);
-});
-
+injectRoutes(router);
 router.init();
