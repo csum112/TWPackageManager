db5249ba39d7d4ca7572ee06a399db0f3ae8fc4d
csum112 <cataremix15@gmail.com>
2020-06-15 23:09:10 +0300

    reverse proxied the three repos

--- shared/server.js
+++ shared/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Repo = require('./repository')
 const app = express();
 
-const PORT = 3000
+const PORT = process.env.port || 8080
 
 app.get("/package", async (req, res) => {
     let page = req.query.page || 0;
