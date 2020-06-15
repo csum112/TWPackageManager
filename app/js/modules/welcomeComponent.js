e27020c2f527190864bf0030d3f10f931beef715
Cristi Rosu <cristi.epic420@gmail.com>
2020-06-15 18:01:07 +0300

    updated welcome page and removed about.hmtl

--- app/js/modules/welcomeComponent.js
+++ app/js/modules/welcomeComponent.js
@@ -16,40 +16,51 @@ export class WelcomeComponent extends HTMLElement {
             }
             
             h1, h3 {
-                color: #fff;
                 font-family: 'Roboto', sans-serif;
                 font-style: normal;
                 font-weight: lighter;
             }
 
             h1 {
+                color: #fff;
                 border-bottom: 2px solid #894EC6;
             }
+
             h3 {
                 color: #894EC6;
             }
 
-            .distros {
+            ul {
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
                 height: 50vh;
+                width: 80vw;
                 display: flex;
                 position: relative;
                 flex-direction: row;
-                justify-content: space-evenly;
+                justify-content: space-between;
                 align-items: center;
-                overflow-y: auto, hidden;
+                overflow-y: auto;
             }
-            
+
+            ul::-webkit-scrollbar {
+                display: none;
+            }
+
             img {
-                margin-top: 5rem;
-                margin-bottom: 5rem;
-                margin-left: 3rem;
-                margin-right: 3rem;
+                padding: 1.5rem;
             }
             
             @media (max-width: 720px) and (orientation: portrait) {
             
-                .distros {
+                ul {
                     flex-direction: column;
+                }
+
+                img {
+                    padding-top: 2rem;
+                    padding-bottom: 2rem;
                 }
             
             }
@@ -60,10 +71,12 @@ export class WelcomeComponent extends HTMLElement {
         <div class="page-content">
             <h1>Welcome</h1>
             <h3>Pick your flavour.</h3>
-            <div class="distros">
-                <a onclick="window.distroService.setDistro('brew')" id="brew"><img src="/assets/png/brew_logo.png" alt="Brew logo" width="250" height="116"></a>
-                <a onclick="window.distroService.setDistro('arch')"><img src="/assets/png/arch_logo.png" alt="Archlinux logo" width="200" height="60"></a>
-            </div>
+            <ul>
+                <li><a onclick="window.distroService.setDistro('brew')" id="brew"><img src="/assets/png/brew_logo.png" alt="Brew logo" width="250" height="116"></a></li>
+                <li><a onclick="window.distroService.setDistro('debian')"><img src="/assets/png/debian_logo.png" alt="Debian logo" width="200" height="100"></a></li>
+                <li><a onclick="window.distroService.setDistro('arch')"><img src="/assets/png/arch_logo.png" alt="Archlinux logo" width="200" height="60"></a></li>
+                <li><a onclick="window.distroService.setDistro('centos')"><img src="/assets/png/centos_logo.png" alt="CentOS logo" width="200" height="67"></a></li>
+            </ul>
         </div>
     `;
 
