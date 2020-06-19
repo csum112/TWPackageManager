5e9c251cc598ded4051e9719fb23a2491b3ff252
csum112 <cataremix15@gmail.com>
2020-06-19 18:13:57 +0300

    ci test

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ docker pull registry.heroku.com/twdemiarch/web
 docker pull registry.heroku.com/twdemidebian/web
 
 # Build docker images
-docker build . -f ./Dockerfile.web -t registry.heroku.com/twdemi/web
-docker build . -f ./Dockerfile.centos -t registry.heroku.com/twdemicentos/web
-docker build . -f ./Dockerfile.arch -t registry.heroku.com/twdemiarch/web
-docker build . -f ./Dockerfile.debian -t registry.heroku.com/twdemidebian/web
+docker build --cache-from registry.heroku.com/twdemi/web . -f ./Dockerfile.web -t registry.heroku.com/twdemi/web
+docker build --cache-from registry.heroku.com/twdemicentos/web . -f ./Dockerfile.centos -t registry.heroku.com/twdemicentos/web
+docker build --cache-from registry.heroku.com/twdemiarch/web . -f ./Dockerfile.arch -t registry.heroku.com/twdemiarch/web
+docker build --cache-from registry.heroku.com/twdemidebian/web . -f ./Dockerfile.debian -t registry.heroku.com/twdemidebian/web
