aea3008b9381417c181b57ea5277f5199bb6ec0b
csum112 <cataremix15@gmail.com>
2020-06-19 18:29:20 +0300

    removed cache

---
+++ README.md
@@ -0,0 +1,2 @@
+## My Developer Machine Installer
+[![Build Status](https://travis-ci.org/csum112/TWPackageManager.svg?branch=alternate)](https://travis-ci.org/csum112/TWPackageManager)
--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 heroku container:login;
-# Pull cache
-docker pull registry.heroku.com/twdemi/web
-docker pull registry.heroku.com/twdemicentos/web
-docker pull registry.heroku.com/twdemiarch/web
-docker pull registry.heroku.com/twdemidebian/web
+
 
 # Build docker images
-docker build --cache-from registry.heroku.com/twdemi/web . -f ./Dockerfile.web -t registry.heroku.com/twdemi/web
-docker build --cache-from registry.heroku.com/twdemicentos/web . -f ./Dockerfile.centos -t registry.heroku.com/twdemicentos/web
-docker build --cache-from registry.heroku.com/twdemiarch/web . -f ./Dockerfile.arch -t registry.heroku.com/twdemiarch/web
-docker build --cache-from registry.heroku.com/twdemidebian/web . -f ./Dockerfile.debian -t registry.heroku.com/twdemidebian/web
+docker build . -f ./Dockerfile.web -t registry.heroku.com/twdemi/web
+docker build . -f ./Dockerfile.centos -t registry.heroku.com/twdemicentos/web
+docker build . -f ./Dockerfile.arch -t registry.heroku.com/twdemiarch/web
+docker build . -f ./Dockerfile.debian -t registry.heroku.com/twdemidebian/web
