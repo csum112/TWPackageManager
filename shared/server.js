aebbb65d416d185eca29d4f750fbfb4ce474599d
Cristi Rosu <cristi.epic420@gmail.com>
2020-06-23 15:02:04 +0300

    frontend fixes and version conflict adjustment

--- shared/server.js
+++ shared/server.js
@@ -39,11 +39,6 @@ console.log(`Starting server on port ${PORT}`)
 app.listen(PORT);
 // parseDep('javapackages-tools').then(deps => console.log(JSON.stringify(deps))).catch(console.log);
 // Repo.getPackage('libxslt').then(console.log).catch(console.log)
-<<<<<<< HEAD
-//parseDep('0ad').then(deps => console.log(JSON.stringify(deps))).catch(console.log);
-//Repo.getPackageVersions('libc6').then(console.log).catch(console.log);
-=======
 // parseDep('0ad').then(deps => console.log(JSON.stringify(deps))).catch(console.log);
 //Repo.getPackageVersions('zlib1g').then(console.log).catch(console.log);
->>>>>>> 551061b8a70ddda086f95c50826c0787e229313f
 process.on('SIGINT', () => process.exit(1));
