2a8005a8cf01d36b3e1797727154e1815c5548fa
csum112 <cataremix15@gmail.com>
2020-06-23 13:13:25 +0300

    trying to speed up travis build

--- build.sh
+++ build.sh
@@ -5,6 +5,12 @@ cd spa
 npm install
 npm run build:prod
 cd ..
+sudo rm -r spa/ app/ srv/
+# Pull latest
+docker pull registry.heroku.com/twdemi/web
+docker pull registry.heroku.com/twdemicentos/web
+docker pull registry.heroku.com/twdemiarch/web
+docker pull registry.heroku.com/twdemidebian/web
 # Build docker images
 docker build . -f ./Dockerfile.web -t registry.heroku.com/twdemi/web
 docker build . -f ./Dockerfile.centos -t registry.heroku.com/twdemicentos/web
